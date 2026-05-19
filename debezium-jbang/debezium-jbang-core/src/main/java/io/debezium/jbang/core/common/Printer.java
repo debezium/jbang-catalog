e3316a2584484668c19011a60d58e8a64dffd4fd
kmos <kmos@commonhaus.dev>
2026-05-19 09:26:36 +0200

    debezium/dbz#1956 introduce printer with stdout
    Signed-off-by: kmos <kmos@commonhaus.dev>

---
+++ debezium-jbang/debezium-jbang-core/src/main/java/io/debezium/jbang/core/common/Printer.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.jbang.core.common;
+
+public interface Printer {
+
+    void println();
+
+    void println(String line);
+
+    void print(String output);
+
+    void printf(String format, Object... args);
+
+    class SystemOutPrinter implements Printer {
+
+        @Override
+        public void println() {
+            // CHECKSTYLE:OFF
+            System.out.println();
+            // CHECKSTYLE:OFF
+            System.out.flush();
+        }
+
+        @Override
+        public void println(String line) {
+            // CHECKSTYLE:OFF
+            System.out.println(line);
+            // CHECKSTYLE:OFF
+            System.out.flush();
+        }
+
+        @Override
+        public void print(String output) {
+            // CHECKSTYLE:OFF
+            System.out.print(output);
+        }
+
+        @Override
+        public void printf(String format, Object... args) {
+            // CHECKSTYLE:OFF
+            System.out.printf(format, args);
+        }
+    }
+}
