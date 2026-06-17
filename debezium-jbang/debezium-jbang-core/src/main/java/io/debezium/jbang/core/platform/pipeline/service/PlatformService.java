bbbf266a4ef18cf40ae2485ea493c8ad2b83e51e
kmos <kmos@commonhaus.dev>
2026-06-17 15:53:59 +0200

    debezium/dbz#2096 use factory
    Signed-off-by: kmos <kmos@commonhaus.dev>

---
+++ debezium-jbang/debezium-jbang-core/src/main/java/io/debezium/jbang/core/platform/pipeline/service/PlatformService.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.jbang.core.platform.pipeline.service;
+
+import java.util.List;
+
+import io.debezium.jbang.core.platform.pipeline.dto.PipelineRequest;
+import io.debezium.jbang.core.platform.pipeline.dto.PipelineResponse;
+
+public interface PlatformService {
+
+    List<PipelineResponse> listPipelines();
+
+    PipelineResponse getPipeline(Long id);
+
+    PipelineResponse createPipeline(PipelineRequest pipelineRequest);
+
+    PipelineResponse updatePipeline(Long id, PipelineRequest pipelineRequest);
+
+    void deletePipeline(Long id);
+}
